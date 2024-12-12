d2e7178157c008a731bd3ec6e7df3a89b1f638d2
smaerd <smj759875821@gmail.com>
2024-12-12 18:24:05 +0800

    Styled Component Props and the css Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import GlobalStyles from './styles/GlobalStyle';
 import Button from './ui/Button';
 import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <div>
         <Button>Hello</Button>
         <Input />
+        <Heading as="h1">xxxxxxxxxxxxxxxxxxx</Heading>
+        <Heading as="h2">xxxxxxxxxxxxxxxxxxx</Heading>
+        <Heading as="h3">xxxxxxxxxxxxxxxxxxx</Heading>
       </div>
     </>
   );

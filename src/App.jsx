97abed147b1e4b26087efc101e328570d0ea31cc
smaerd <smj759875821@gmail.com>
2024-12-20 16:28:20 +0800

    Building More Reusable Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import GlobalStyles from './styles/GlobalStyle';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <div>
-        <Button>Hello</Button>
-        <Input />
-        <Heading as="h1">xxxxxxxxxxxxxxxxxxx</Heading>
-        <Heading as="h2">xxxxxxxxxxxxxxxxxxx</Heading>
-        <Heading as="h3">xxxxxxxxxxxxxxxxxxx</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">xxxxxxxxxxxxxxxxxxx</Heading>
+            <div>
+              <Heading as="h2">xxxxxxxxxxxxxxxxxxx</Heading>
+              <Button>Hello1</Button>
+              <Button variation="secondary" size="small">
+                Hello2
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">xxxxxxxxxxxxxxxxxxx</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </div>
     </>
   );
